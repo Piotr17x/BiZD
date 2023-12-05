a90b98411d6ea9e0911adc045a1fb4b9fcba2898
Piotr17x <59728942+Piotr17x@users.noreply.github.com>
2023-12-05 17:41:17 +0100

    Update stat-3.py

--- stat-3.py
+++ stat-3.py
@@ -1,8 +1,5 @@
 import scipy.stats as scs
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import gaussian_kde
 
 
 def zad_1():
@@ -23,4 +20,70 @@ def zad_2():
 
 
 zad_2()
+print('=+=+=+=+=+=+=')
+
+
+def zad_3():
+    df = pd.read_csv('napoje.csv', sep=';')
+    test_results = scs.normaltest(df)
+    new_df = pd.DataFrame([[round(x, 3) for x in test_results.pvalue]], columns=df.columns)
+    print(new_df)
+
+
+zad_3()
+print('=+=+=+=+=+=+=')
+
+
+def zad_4():
+    df = pd.read_csv('napoje.csv', sep=';')
+    print('rowność średnich')
+    results = scs.ttest_ind(df['okocim'], df['lech'])
+    print(f'okocim - lech: {results.pvalue:.3f}')
+    results = scs.ttest_ind(df['fanta '], df['regionalne'])
+    print(f'fanta - regionalne: {results.pvalue:.3f}')
+    results = scs.ttest_ind(df['cola'], df['pepsi'])
+    print(f'cola - pepsi: {results.pvalue:.3f}')
+
+
+zad_4()
+print('=+=+=+=+=+=+=')
+
+
+def zad_5():
+    df = pd.read_csv('napoje.csv', sep=';')
+    print('rowność wariancji')
+    results = scs.levene(df['okocim'], df['lech'])
+    print(f'okocim - lech: {results.pvalue:.3f}')
+    results = scs.levene(df['żywiec'], df['fanta '])
+    print(f'żywiec - fanta: {results.pvalue:.3f}')
+    results = scs.levene(df['regionalne'], df['cola'])
+    print(f'regionalne - cola: {results.pvalue:.3f}')
+
+
+zad_5()
+print('=+=+=+=+=+=+=')
+
+
+def zad_6():
+    df = pd.read_csv('napoje.csv', sep=';')
+    print('równość średnich pomiędzy latami 2001 i 2015 dla piw regionalnych')
+    print(f"{scs.ttest_ind(df[df['rok'] == 2001]['regionalne'], df[df['rok'] == 2015]['regionalne']).pvalue:.5f}")
+
+
+zad_6()
+print('=+=+=+=+=+=+=')
+
+
+def zad_7():
+    df = pd.read_csv('napoje.csv', sep=';')
+    df2 = pd.read_csv('napoje_po_reklamie.csv', sep=';')
+    print('cola')
+    print(f"{scs.ttest_rel(df[df['rok'] == 2016]['cola'], df2['cola']).pvalue:.5f}")
+    print('fanta')
+    print(f"{scs.ttest_rel(df[df['rok'] == 2016]['fanta '], df2['fanta ']).pvalue:.5f}")
+    print('pepsi')
+    print(f"{scs.ttest_rel(df[df['rok'] == 2016]['pepsi'], df2['pepsi']).pvalue:.5f}")
+
+
+zad_7()
 print('=+=+=+=+=+=+=')
